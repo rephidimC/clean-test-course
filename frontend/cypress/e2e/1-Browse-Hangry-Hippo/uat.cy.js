efbed38e0f067803f977bd148b00c0d1dcc2f9ee
quasifu <brian@cheng.ninja>
2022-08-09 12:52:46 -0500

    lesson 6

---
+++ frontend/cypress/e2e/1-Browse-Hangry-Hippo/uat.cy.js
@@ -0,0 +1,11 @@
+describe('Shows Appeteasers', () => {
+  it('Connect to Dev Server', () => {
+    cy.visit('http://hangry-web-dev.us-east-1.elasticbeanstalk.com/');
+  });
+  it('selects Handhelds', () => {
+    //TODO: Add a test to check Handhelds
+  });
+  it('selects Appeteasers', () => {
+    //TODO: Add a test to check Appeteasers
+  });
+});
