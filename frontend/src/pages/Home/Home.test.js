2de0df706c758edae62ff873fd35746491b4896b
Victor Afolayan <adelekeafolayan@gmail.com>
2025-09-29 19:10:30 +0100

    :zap: update

--- frontend/src/pages/Home/Home.test.js
+++ frontend/src/pages/Home/Home.test.js
@@ -1,30 +1,34 @@
-import { render, screen, waitFor } from '@testing-library/react';
-import { API_URL } from '../../utils/constants';
-import axios from 'axios';
-import Home from '.';
+import { render, screen } from "@testing-library/react";
+import { API_URL } from "../../utils/constants";
+import axios from "axios";
+import Home from ".";
 
-describe('Test Home', () => {
-  test('Test Render', async () => {
+describe("Test Home", () => {
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  test("Test Render", async () => {
     //Arrange: Setup the mock API
     //Listen for any GET requests using the axios module
-    const mockGet = jest.spyOn(axios, 'get');
+    const mockGet = jest.spyOn(axios, "get");
     //Intercept the GET requests and provide a mocked response
     mockGet.mockImplementation((url) => {
       switch (url) {
         case `${API_URL}/api/category/?format=json`:
           return Promise.resolve({
             data: {
-              status: 'success',
+              status: "success",
               data: [
                 {
                   id: 1,
-                  name: 'Handhelds',
+                  name: "Handhelds",
                   description: "So big, you don't need thumbs.",
                 },
                 {
                   id: 2,
-                  name: 'Appeteasers',
-                  description: 'Tease the hangry hippo, he get hangrier',
+                  name: "Appeteasers",
+                  description: "Tease the hangry hippo, he get hangrier",
                 },
               ],
             },
@@ -32,7 +36,7 @@ describe('Test Home', () => {
         default:
           return Promise.resolve({
             data: {
-              status: 'fail',
+              status: "fail",
             },
           });
       }
@@ -45,6 +49,18 @@ describe('Test Home', () => {
     //There should be 2 categories as defined in the mock response above
     expect(await screen.findAllByTestId(/category-item/i)).toHaveLength(2);
     //The word Appeateasers should be in there as defined in the mock response above.
-    expect(await screen.findByText('Appeteasers')).toBeInTheDocument();
+    expect(await screen.findByText("Appeteasers")).toBeInTheDocument();
+  });
+
+  //Copied "Test Render"
+  test("Test Integration Render", async () => {
+    //Deleted the mock calls.  There is no Arrange for this test since this will access the shared servers.
+    //Act: Call the Home page
+    render(<Home />);
+    //Assert: Check the values in the rendered Home page.
+    //There should be 2 categories as defined in the mock response above
+    expect(await screen.findAllByTestId(/category-item/i)).toHaveLength(2);
+    //The word Appeateasers should be in there as defined in the mock response above.
+    expect(await screen.findByText("Appeteasers")).toBeInTheDocument();
   });
 });
