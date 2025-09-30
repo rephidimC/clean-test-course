2f1cd8472e3ff6a984108e60c2edadf5f930f295
Victor Afolayan <adelekeafolayan@gmail.com>
2025-09-30 01:23:46 +0100

    :zap: update

--- frontend/cypress/e2e/1-Browse-Hangry-Hippo/uat.cy.js
+++ frontend/cypress/e2e/1-Browse-Hangry-Hippo/uat.cy.js
@@ -1,11 +1,19 @@
-describe('Shows Appeteasers', () => {
-  it('Connect to Dev Server', () => {
-    cy.visit('https://hangryhippo.quantic.host/');
+describe("Shows Appeteasers", () => {
+  it("Connect to Dev Server", () => {
+    cy.visit("https://hangryhippo.quantic.host/");
   });
-  it('selects Handhelds', () => {
+  it("selects Handhelds", () => {
     //TODO: Add a test to check Handhelds
+    cy.contains("Handhelds").click();
+    cy.contains("Cheese Burger");
+    cy.contains("Fajita Tacos");
   });
-  it('selects Appeteasers', () => {
+  it("selects Appeteasers", () => {
     //TODO: Add a test to check Appeteasers
+    cy.contains("Appeteasers").click();
+    cy.contains("Tater Tots");
+    cy.contains("Buffalo Wings");
+    cy.contains("Cheese Burger").should("not.exist");
+    cy.contains("Fajita Tacos").should("not.exist");
   });
 });
